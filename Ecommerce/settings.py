67aa69ee13e441e62e511288febbe3a80aa21026
Otavio Schmieleski <otavioschmieleski@gmail.com>
2023-10-26 13:48:19 -0300

    criando models dos pedidos

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'produto',
+    'pedido',
 
     #TODO: remover debug toolbar
     "debug_toolbar",
--- pedido/models.py
+++ pedido/models.py
@@ -1,3 +1,67 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+""" 
+Pedido:
+    user - FK User
+    total - Float
+    status - Choices
+        ('A', 'Aprovado'),
+        ('C', 'Criado'),
+        ('R', 'Reprovado'),
+        ('P', 'Pendente'),
+        ('E', 'Enviado'),
+        ('F', 'Finalizado'),
+"""
+class Pedido(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    total = models.FloatField()
+    status = models.CharField( 
+        default="C",
+        max_length=1,
+        choices=(
+            ('A', 'Aprovado'),
+            ('C', 'Criado'),
+            ('R', 'Reprovado'),
+            ('P', 'Pendente'),
+            ('E', 'Enviado'),
+            ('F', 'Finalizado'),
+        )
+    )
+
+    def __str__(self):
+        return f'Pedido N. {self.pk}'
+    
+
+"""
+ItemPedido:
+    pedido - FK pedido
+    produto - Char
+    produto_id - Int
+    variacao - Char
+    variacao_id - Int
+    preco - Float
+    preco_promocional - Float
+    quantidade - Int
+    imagem - Char
+"""
+
+class ItemPedido(models.Model):
+    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    produto = models.CharField(max_length=255)
+    produto_id = models.PositiveIntegerField()
+    variacao = models.CharField(max_length=255)
+    variacao_id = models.PositiveIntegerField()
+    preco = models.FloatField()
+    preco_promocional = models.FloatField(default=0)
+    quantidade = models.PositiveIntegerField()
+    imagem = models.CharField(max_length=2000)
+
+    def __str__(self):
+        return f'Item do {self.pedido}'
+
+    class Meta:
+        verbose_name = 'Item do pedido'
+        verbose_name_plural = 'Itens do pedido'
+
+    
