6e1d4b156be8ec242530ca4239f70cad09a9ed73
Otavio Schmieleski <otavioschmieleski@gmail.com>
2023-10-27 14:25:56 -0300

    configurando views e templates

--- produto/views.py
+++ produto/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views import View
 from django.http import HttpResponse
+from . import models
+
 # Create your views here.
 
-class ListaProdutos(View):
-    def get(Self,*args, **kwargs):
-        return HttpResponse('ListaProdutos')
+class ListaProdutos(ListView):
+    model = models.Produto
+    template_name = 'produtos/lista.html'
+    context_object_name = 'produtos'
 
 class DetalheProdutos(View):
     def get(Self,*args, **kwargs):
