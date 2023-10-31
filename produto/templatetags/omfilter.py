4e782571c06314b3f5fcf23ca6f77bb9da4c4595
Otavio Schmieleski <otavioschmieleski@gmail.com>
2023-10-31 16:03:20 -0300

    configurando o carrinho do site

--- produto/templatetags/omfilter.py
+++ produto/templatetags/omfilter.py
@@ -1,7 +1,11 @@
 from django.template import Library
-
+from utils import utils
 register = Library()
 
 @register.filter
 def formata_preco(valor):
-    return f"R$ {valor:.2f}".replace('.',',')
+    return utils.formata_preco(valor)
+
+@register.filter
+def cart_total_qtd(carrinho):
+    return utils.cart_total_qtd(carrinho)
--- produto/urls.py
+++ produto/urls.py
@@ -7,8 +7,7 @@ urlpatterns = [
     path('',views.ListaProdutos.as_view(), name="lista"),
     path('<slug>',views.DetalheProdutos.as_view(), name="detalhe"),
     path('adicionaraocarrinho/',views.AdicionarAoCarrinho.as_view(), name="adicionaraocarrinho"),
-    path('removerDocarrinho/',views.RemoverDoCarrinho.as_view(), name="removerdocarrinho"),
-    path('removerDocarrinho/',views.RemoverDoCarrinho.as_view(), name="removerdocarrinho"),
+    path('removerdocarrinho/',views.RemoverDoCarrinho.as_view(), name="removerdocarrinho"),
     path('carrinho/',views.Carrinho.as_view(), name="carrinho"),
     path('finalizar/',views.Finalizar.as_view(), name="finalizar"),
 
